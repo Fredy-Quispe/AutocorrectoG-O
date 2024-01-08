6e82f726d393f13b0bec95c6888cd54285812390
Fredy Quispe <frquispema@est.unap.edu.pe>
2024-01-08 01:34:36 -0500

    Create header and add styling

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <AppHeader />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from '@/components/AppHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    AppHeader,
+  },
+};
 </script>
 
 <style>
@@ -21,6 +22,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  background-color: #d9fadc;
+  margin: 0;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <header class="app-header">
+    <div class="logo-container">
+      <img src="@/assets/logo.png" alt="Logo" class="logo" />
+    </div>
+    <div class="title-container">
+      <h1 class="title">Analizar documento PDF</h1>
+    </div>
+    <div class="tools-container">
+      <p class="tools-text">Herramientas</p>
+    </div>
+  </header>
+</template>
+  
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
+  
+<style scoped>
+.app-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  background-color: white;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1); 
+}
+
+.logo {
+  max-width: 40px;
+}
+
+.title {
+  margin: 0;
+}
+
+.tools-container {
+  overflow: hidden;
+  margin-right: 40px;
+}
+
+.tools-text {
+  margin: 0;
+  font-weight: italic;
+  white-space: nowrap;
+}
+</style>
+  
