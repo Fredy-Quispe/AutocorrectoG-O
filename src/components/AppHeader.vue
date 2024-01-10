709ac1564d965d4db6fb55b1e24808acf33b2fac
Fredy Quispe <frquispema@est.unap.edu.pe>
2024-01-10 00:28:02 -0500

    Modify AppHeader file and Create FileUpload component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,15 @@
 <template>
   <header class="app-header">
     <div class="logo-container">
-      <img src="@/assets/logo.png" alt="Logo" class="logo" />
+      <img src="@/assets/logo.svg" alt="Logo" class="logo" />
+      <span class="logo-text">PDF Analyze</span>
     </div>
     <div class="title-container">
-      <h1 class="title">Analizar documento PDF</h1>
+      <font-awesome-icon :icon="['fas', 'file-pdf']" class="title-ico"/>
+      <h1 class="title-text">Analizar documento PDF</h1>
     </div>
     <div class="tools-container">
+      <img src="@/assets/square.svg" alt="Square" class="square" />
       <p class="tools-text">Herramientas</p>
     </div>
   </header>
@@ -23,33 +26,64 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 10px;
   background-color: white;
   width: 100%;
+  height: 85px;
   position: fixed;
   top: 0;
   left: 0;
   z-index: 1000;
-  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1); 
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
 }
-
+.logo-container {
+  display: flex;
+  align-items: center;
+  margin: 0 20px;
+}
 .logo {
-  max-width: 40px;
+  width: 50px;
 }
-
-.title {
+.logo-text {
+  display: inline-block;
+  vertical-align: middle;
+  font-weight: 700;
+  font-size: 22px;
+  line-height: 29px;
+  color: #313131;
+}
+.title-container {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+.title-ico {
+  color: #4caf50;
+  font-size: 32px;
+}
+.title-text {
   margin: 0;
+  font-size: 26px;
+  line-height: 34px;
+  color: #313131;
 }
-
 .tools-container {
+  display: flex;
+  align-items: center;
+  width: 180px;
   overflow: hidden;
-  margin-right: 40px;
+  margin: 0 20px;
+  gap: 5px;
 }
-
+.square {
+  width: 13px;
+  height: 13px;
+}
 .tools-text {
   margin: 0;
-  font-weight: italic;
   white-space: nowrap;
+  font-size: 14px;
+  line-height: 25px;
+  color: #313131;
 }
 </style>
   
---
+++ src/components/FileUpload.vue
@@ -0,0 +1,94 @@
+<template>
+    <section class="main-content">
+      <div class="section-title">
+        <span>Analize en segundos la gramatica y ortografia de su documento PDF.</span>
+      </div>
+      <button class="main-button">Seleccionar documento PDF</button>
+      <div class="alternative-buttons">
+        <div class="circular-buttons">
+          <button class="alternative-button circular" title="Seleccionar un archivo de Google Drive"><font-awesome-icon :icon="['fab', 'google-drive']" /></button>
+          <button class="alternative-button circular" title="Seleccione el archivo de Dropbox"><font-awesome-icon :icon="['fab', 'dropbox']" /></button>
+          <button class="alternative-button circular" title="Cargar el archivo desde la URL"><font-awesome-icon :icon="['fas', 'link']" /></button>
+        </div>
+      </div>
+      <div class="how-to-use">
+        <h2>Cómo analizar un documento PDF</h2>
+        <div class="step-cards">
+          <div class="step-card">Paso 1: Elija el PDF que desea analizar desde su ordenador.</div>
+          <div class="step-card">Paso 2: Nuestro analizador de ortografia y gramatica comenzará a extraer el texto de su PDF.</div>
+          <div class="step-card">Paso 3: En segundos se crea un documento PDF donde se resaltan los errores gramaticales y ortograficos y listo para descargar.</div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'FileUpload',
+};
+</script>
+
+<style scoped>
+.main-content {
+  margin-top: 170px;
+}
+.main-button {
+  background-color: #4caf50;
+  color: white;
+  padding: 15px 30px;
+  font-size: 18px;
+  border: none;
+  border-radius: 50px;
+  cursor: pointer;
+  margin-top: 50px;
+  height: 73px;
+  width: 600px;
+}
+.main-button:hover {
+  background-color: #45a049;
+}
+.alternative-buttons {
+  margin-top: 30px;
+  text-align: center;
+}
+.alternative-button {
+  background-color: white;
+  border: 1px solid rgb(151, 151, 151);
+  outline: 2px solid transparent; 
+  color: black;
+  padding: 10px 20px;
+  font-size: 20px;
+  cursor: pointer;
+  margin-right: 16px;
+}
+.alternative-button:hover {
+  border-color: #45a049;
+}
+.circular-buttons {
+  display: flex;
+  justify-content: center;
+}
+.circular {
+  border-radius: 50%;
+  width: 45px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 14px;
+}
+.how-to-use {
+  margin-top: 80px;
+}
+.step-cards {
+  display: flex;
+}
+.step-card {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+  margin: 10px 10px 10px 10px;
+  display: inline-block;
+}
+</style>
